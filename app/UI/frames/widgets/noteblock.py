c63bdf0ba8b2e96793242f116247d6c6836813c0
Felipe Sampaio <felipesampaiocscj@gmail.com>
2026-02-14 18:44:42 -0300

    add open and save function on noteblock

--- app/UI/frames/widgets/noteblock.py
+++ app/UI/frames/widgets/noteblock.py
@@ -1,12 +1,38 @@
 import customtkinter as ctk
 from ..colors.colors import *
+from tkinter import filedialog
+
+global texto
+
+def openFile():
+    global texto
+    caminho = filedialog.askopenfilename(
+        title="Abrir arquivo",
+        filetypes=[("Arquivos de texto", "*.txt")]
+    )
+    with open(caminho,'r',encoding='utf-8') as arquivo:
+        conteudo = arquivo.read()
+        texto.insert('0.0',conteudo)
+
+def saveFile():
+    global texto
+    caminho = filedialog.asksaveasfilename(
+        title="Salvar arquivo",
+        defaultextension=".txt",
+        filetypes=[("Text files", "*.txt")]
+    )
+    
+    if caminho:
+        with open(caminho, "w", encoding="utf-8") as f:
+            f.write(texto.get("0.0", ctk.END))
 
 def noteblock(app):
+    global texto
     texto = ctk.CTkTextbox(app, state='normal',fg_color=FRAMES)
     texto.pack(fill='both', expand=True)
 
-    save_btn = ctk.CTkButton(app,text="Salvar",fg_color=BUTTON)
-    open_btn = ctk.CTkButton(app,text="Abrir",fg_color=BUTTON)
+    save_btn = ctk.CTkButton(app,text="Salvar",fg_color=BUTTON,command=saveFile)
+    open_btn = ctk.CTkButton(app,text="Abrir",fg_color=BUTTON,command=openFile)
 
     save_btn.pack(side='right',pady=4)
     open_btn.pack(side='left',pady=4)
